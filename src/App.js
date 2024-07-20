ce9612d56b62f245d3ce03834de8f6d03ba17ba0
nguyenthanhtien3214 <nguyenthanhtien3214@gmail.com>
2024-07-21 01:09:59 +0700

    Update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tien nguyen
         </p>
         <a
           className="App-link"
