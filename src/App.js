d503dbb2224e6adf901693453bebda5f6273885c
nguyenthanhtien3214 <nguyenthanhtien3214@gmail.com>
2024-07-21 01:18:00 +0700

    cap nhat lan 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tien nguyen
+          Tien nguyen 123
         </p>
         <a
           className="App-link"
